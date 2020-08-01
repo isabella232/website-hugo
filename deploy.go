a70fa2998ff0dc26e9ccfe2ce8272e252541fda3
Renan DelValle <commit@ridv.xyz>
2020-07-31 18:48:56 -0700

    Adding deploy script.

---
+++ deploy.go
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+# Build the project.
+hugo
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
